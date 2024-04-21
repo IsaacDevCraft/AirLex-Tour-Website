5fb2f2775cc33dc2c2dd7d24ce4b59615117d98e
IsaacDevCraft <isaacoguns9@gmail.com>
2024-04-21 20:45:59 +0200

    updated registration details

--- javascript/registration.js
+++ javascript/registration.js
@@ -1,23 +1,23 @@
 var form = document.getElementById("myForm"),
     imgInput = document.querySelector(".img"),
     file = document.getElementById("imgInput"),
-    userName = document.getElementById("name"),
+    passengerName = document.getElementById("name"),
     age = document.getElementById("age"),
-    city = document.getElementById("city"),
-    email = document.getElementById("email"),
-    phone = document.getElementById("phone"),
-    post = document.getElementById("post"),
-    sDate = document.getElementById("sDate"),
+    nationality = document.getElementById("city"), 
+    nextOfKinEmail = document.getElementById("email"), 
+    nextOfKinPhone = document.getElementById("phone"), 
+    seatNo = document.getElementById("post"), 
+    departureDate = document.getElementById("sDate"), 
     submitBtn = document.querySelector(".submit"),
-    userInfo = document.getElementById("data"),
+    passengerInfo = document.getElementById("data"),
     modal = document.getElementById("userForm"),
     modalTitle = document.querySelector("#userForm .modal-title"),
     newUserBtn = document.querySelector(".newUser");
 
-let getData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')) : [];
+let passengerData = localStorage.getItem('passengerProfile') ? JSON.parse(localStorage.getItem('passengerProfile')) : [];
 
 let isEdit = false, editId;
-showInfo();
+showPassengerInfo();
 
 newUserBtn.addEventListener('click', ()=> {
     submitBtn.innerText = 'Submit';
@@ -43,68 +43,66 @@ file.onchange = function(){
     }
 };
 
-function showInfo(){
-    // Remove existing elements
-    document.querySelectorAll('.employeeDetails').forEach(info => info.remove());
+function showPassengerInfo(){
+   
+    document.querySelectorAll('.passengerDetails').forEach(info => info.remove());
 
-    // Add new elements with the 'text-white' class
-    getData.forEach((element, index) => {
-        let createElement = `<tr class="employeeDetails text-white">
+    passengerData.forEach((element, index) => {
+        let createElement = `<tr class="passengerDetails text-white">
             <td>${index+1}</td>
             <td><img src="${element.picture}" alt="" width="50" height="50"></td>
-            <td>${element.employeeName}</td>
-            <td>${element.employeeAge}</td>
-            <td>${element.employeeCity}</td>
-            <td>${element.employeeEmail}</td>
-            <td>${element.employeePhone}</td>
-            <td>${element.employeePost}</td>
-            <td>${element.startDate}</td>
-
+            <td>${element.passengerName}</td>
+            <td>${element.age}</td>
+            <td>${element.nationality}</td> 
+            <td>${element.nextOfKinEmail}</td> 
+            <td>${element.nextOfKinPhone}</td> 
+            <td>${element.seatNo}</td> 
+            <td>${element.departureDate}</td> 
             <td>
-                <button class="btn btn-success" onclick="readInfo('${element.picture}', '${element.employeeName}', '${element.employeeAge}', '${element.employeeCity}', '${element.employeeEmail}', '${element.employeePhone}', '${element.employeePost}', '${element.startDate}')" data-bs-toggle="modal" data-bs-target="#readData"><i class="bi bi-eye"></i></button>
+                <button class="btn btn-success" onclick="readPassengerInfo('${element.picture}', '${element.passengerName}', '${element.age}', '${element.nationality}', '${element.nextOfKinEmail}', '${element.nextOfKinPhone}', '${element.seatNo}', '${element.departureDate}')" data-bs-toggle="modal" data-bs-target="#readData"><i class="bi bi-eye"></i></button>
 
-                <button class="btn btn-primary" onclick="editInfo(${index}, '${element.picture}', '${element.employeeName}', '${element.employeeAge}', '${element.employeeCity}', '${element.employeeEmail}', '${element.employeePhone}', '${element.employeePost}', '${element.startDate}')" data-bs-toggle="modal" data-bs-target="#userForm"><i class="bi bi-pencil-square"></i></button>
+                <button class="btn btn-primary" onclick="editPassengerInfo(${index}, '${element.picture}', '${element.passengerName}', '${element.age}', '${element.nationality}', '${element.nextOfKinEmail}', '${element.nextOfKinPhone}', '${element.seatNo}', '${element.departureDate}')" data-bs-toggle="modal" data-bs-target="#userForm"><i class="bi bi-pencil-square"></i></button>
 
-                <button class="btn btn-danger" onclick="deleteInfo(${index})"><i class="bi bi-trash"></i></button>    
+                <button class="btn btn-danger" onclick="deletePassengerInfo(${index})"><i class="bi bi-trash"></i></button>    
             </td>
         </tr>`;
 
-        userInfo.innerHTML += createElement;
+        passengerInfo.innerHTML += createElement;
     });
 }
 
-function readInfo(pic, name, age, city, email, phone, post, sDate){
+function readPassengerInfo(pic, name, age, nationality, email, phone, seatNo, departureDate){
     document.querySelector('.showImg').src = pic;
     document.querySelector('#showName').value = name;
     document.querySelector("#showAge").value = age;
-    document.querySelector("#showCity").value = city;
+    document.querySelector("#showNationality").value = nationality;
     document.querySelector("#showEmail").value = email;
     document.querySelector("#showPhone").value = phone;
-    document.querySelector("#showPost").value = post;
-    document.querySelector("#showsDate").value = sDate;
+    document.querySelector("#showPost").value = seatNo;
+    document.querySelector("#showsDate").value = departureDate;
 }
 
-function editInfo(index, pic, name, Age, City, Email, Phone, Post, Sdate){
+function editPassengerInfo(index, pic, name, Age, Nationality, Email, Phone, Post, Sdate){
     isEdit = true;
     editId = index;
     imgInput.src = pic;
-    userName.value = name;
+    passengerName.value = name;
     age.value = Age;
-    city.value =City;
-    email.value = Email;
-    phone.value = Phone;
-    post.value = Post;
-    sDate.value = Sdate;
+    nationality.value = Nationality;
+    nextOfKinEmail.value = Email;
+    nextOfKinPhone.value = Phone;
+    seatNo.value = Post;
+    departureDate.value = Sdate;
 
     submitBtn.innerText = "Update";
     modalTitle.innerText = "Update The Form";
 }
 
-function deleteInfo(index){
+function deletePassengerInfo(index){
     if(confirm("Are you sure want to delete?")){
-        getData.splice(index, 1);
-        localStorage.setItem("userProfile", JSON.stringify(getData));
-        showInfo();
+        passengerData.splice(index, 1);
+        localStorage.setItem("passengerProfile", JSON.stringify(passengerData));
+        showPassengerInfo();
     }
 }
 
@@ -112,30 +110,30 @@ form.addEventListener('submit', (e)=> {
     e.preventDefault();
 
     const information = {
-        picture: imgInput.src == undefined ? "./image/Profile Icon.webp" : imgInput.src,
-        employeeName: userName.value,
-        employeeAge: age.value,
-        employeeCity: city.value,
-        employeeEmail: email.value,
-        employeePhone: phone.value,
-        employeePost: post.value,
-        startDate: sDate.value
+        picture: imgInput.src == undefined ? "/images/profile-icon.jpeg" : imgInput.src,
+        passengerName: passengerName.value,
+        age: age.value,
+        nationality: nationality.value,
+        nextOfKinEmail: nextOfKinEmail.value,
+        nextOfKinPhone: nextOfKinPhone.value,
+        seatNo: seatNo.value,
+        departureDate: departureDate.value
     };
 
     if(!isEdit){
-        getData.push(information);
+        passengerData.push(information);
     }
     else{
         isEdit = false;
-        getData[editId] = information;
+        passengerData[editId] = information;
     }
 
-    localStorage.setItem('userProfile', JSON.stringify(getData));
+    localStorage.setItem('passengerProfile', JSON.stringify(passengerData));
 
     submitBtn.innerText = "Submit";
     modalTitle.innerHTML = "Fill The Form";
 
-    showInfo();
+    showPassengerInfo();
 
     form.reset();
 
